c5ced6beaacb260071722701615e28ef481aeef9
Tom Fischer <fischertom@outlook.com>
2023-05-10 13:30:28 +0200

    Clean Test Set

--- dataset/FlyingThings.py
+++ dataset/FlyingThings.py
@@ -25,7 +25,7 @@ class FlyingThingsDataset(Dataset):
         self.transform = transform
         self.flow_transform = transforms.CenterCrop(384)
         self.density = density
-        path = '/share_chairilg/data/FlyingThings3d_full/out_json/optical_flow_files/optical_flow_train_finalpass.json' if self.mode == 'train' else '/share_chairilg/data/FlyingThings3d_full/out_json/optical_flow_files/optical_flow_test_finalpass.json'
+        path = '/share_chairilg/data/FlyingThings3d_full/out_json/optical_flow_files/optical_flow_train_finalpass.json' if self.mode == 'train' else '/share_chairilg/data/FlyingThings3d/out_json/val_left_forward_flow.json'
         self.ds = Dataset(path).read()
 
     def __len__(self):
@@ -33,13 +33,23 @@ class FlyingThingsDataset(Dataset):
 
     def __getitem__(self, idx):
         item = self.ds.__getitem__(idx)
-
-        im0 = item['left0'].data()
-        im1 = item['left1'].data()
-        flow = item['left_forward'].data()
-        if self.transform:
-            im0 = self.transform(np.array(im0[:,:,:3]))
-            im1 = self.transform(np.array(im1[:,:,:3]))
-            flow = self.flow_transform(torch.Tensor(np.nan_to_num(flow[:,:,:2])).permute(2, 0, 1))
-        c, h, w = im1.shape
-        return im0, im1, (torch.FloatTensor(1, h, w).uniform_() > self.density).float(), flow
+        if self.mode == 'train':
+            im0 = item['left0'].data()
+            im1 = item['left1'].data()
+            flow = item['left_forward'].data()
+            if self.transform:
+                im0 = self.transform(np.array(im0[:,:,:3]))
+                im1 = self.transform(np.array(im1[:,:,:3]))
+                flow = self.flow_transform(torch.Tensor(np.nan_to_num(flow[:,:,:2])).permute(2, 0, 1))
+            c, h, w = im1.shape
+            return im0, im1, (torch.FloatTensor(1, h, w).uniform_() > self.density).float(), flow
+        else:
+            im0 = item['image0'].data()
+            im1 = item['image1'].data()
+            flow = item['flow0'].data()
+            if self.transform:
+                im0 = self.transform(np.array(im0))
+                im1 = self.transform(np.array(im1))
+                flow = self.flow_transform(torch.Tensor(np.nan_to_num(flow)).permute(2, 0, 1))
+            c, h, w = im1.shape
+            return im0, im1, (torch.FloatTensor(1, h, w).uniform_() > self.density).float(), flow
