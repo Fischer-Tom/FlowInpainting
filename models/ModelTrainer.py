b911cd1d478db1d6d064b8cb7f700ce128faceef
Tom Fischer <fischertom@outlook.com>
2023-05-15 14:24:04 +0200

    fp16 training

--- models/ModelTrainer.py
+++ models/ModelTrainer.py
@@ -78,9 +78,8 @@ class ModelTrainer:
                 break
             if not torch.is_tensor(predict_flow):
                 predict_flow = predict_flow[0]
-            print(start.elapsed_time(end))
             Flow_vis = flow_vis.flow_to_color(Flow[0].detach().cpu().permute(1,2,0).numpy())
-            Pred_vis = flow_vis.flow_to_color(predict_flow[0].detach().cpu().permute(1, 2, 0).numpy())
+            Pred_vis = flow_vis.flow_to_color(torch.nan_to_num_(predict_flow[0]).detach().cpu().permute(1, 2, 0).numpy())
             I1_vis = inverse_normalize(I1[0].to(torch.float32).cpu())
             Masked_vis = flow_vis.flow_to_color(Masked_Flow[0].detach().cpu().permute(1, 2, 0).numpy())
             Mask_vis = torch.cat((Mask[0],Mask[0],Mask[0]),dim=0).detach().cpu()
@@ -116,7 +115,7 @@ class ModelTrainer:
                 running_loss += batch_risk.item()
                 iterations += 1
         Flow_vis = flow_vis.flow_to_color(Flow[0].detach().cpu().permute(1,2,0).numpy())
-        Pred_vis = flow_vis.flow_to_color(predict_flow[0].detach().cpu().permute(1, 2, 0).numpy())
+        Pred_vis = flow_vis.flow_to_color(torch.nan_to_num_(predict_flow[0]).detach().cpu().permute(1, 2, 0).numpy())
         Masked_vis = flow_vis.flow_to_color(Masked_Flow[0].detach().cpu().permute(1, 2, 0).numpy())
         I1_vis = inverse_normalize(I1[0].detach().cpu())
         Mask_vis = torch.cat((Mask[0],Mask[0],Mask[0]),dim=0).detach().cpu()
